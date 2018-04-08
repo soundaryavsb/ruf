3d0d6cf3ea1ff6cfe87e34f90be9e53cad5dadd4
soundaryavsb <31506228+soundaryavsb@users.noreply.github.com>
2018-04-08 12:21:03 +0530

    Create exceptionHandling.cpp

---
+++ exceptionHandling.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+ 
+int main() {
+try
+{
+	int num1;
+	cout<<"Enter first number:"<<endl;
+	cin>>num1;
+		int num1;
+	cout<<"Enter first number:"<<endl;
+	cin>>num1;
+	if(num2==0)
+	{
+		throw num2;
+	}
+	else
+	cout<<num1/num2<<endl;
+}catch(int x)
+{
+	cout<<"Unfortunately some Error occured\n";
+	cout<<"ERROR CODE"<<x;
+}
+	return 0;
+}
